f70a1e7e8a1f24cb43004c805fab67d592fd8cea
Anastasia Pavlush <n.pavlush16@gmail.com>
2024-05-27 23:44:55 +0300

    added dynamic content to the tamplates

--- index.js
+++ index.js
@@ -17,8 +17,11 @@ app.set('views', `${__dirname}/views`)
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
-app.get('/', (req, res) => {
-    res.render('index')
+app.get('/', async (req, res) => {
+    const posts = await Post.find({})
+    res.render('index', {
+        posts
+    })
 })
 
 app.get('/posts/new', (req, res) => {
@@ -39,8 +42,11 @@ app.get('/about', (req, res) => {
     res.render('about')
 })
 
-app.get('/post', (req, res) => {
-    res.render('post')
+app.get('/post/:id', async (req, res) => {
+    const post = await Post.findById(req.params.id)
+    res.render('post', {
+        post
+    })
 })
 
 app.get('/contact', (req, res) => {
