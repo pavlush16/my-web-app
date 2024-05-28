b06c7518b21a6a765767fe20fd06d37db0e8afa3
Anastasia Pavlush <n.pavlush16@gmail.com>
2024-05-28 13:42:23 +0300

    added register and login

--- controllers/homePage.js
+++ controllers/homePage.js
@@ -2,6 +2,9 @@ const Post = require('../database/models/Post')
 
 module.exports = async (req, res) => {
     const posts = await Post.find({})
+
+    console.log(req.session)
+
     res.render('index', {
         posts
     })
---
+++ database/models/User.js
@@ -0,0 +1,35 @@
+const bcrypt = require('bcryptjs');
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+});
+
+UserSchema.pre('save', function(next) {
+    const user = this;
+
+    // Check if the password has been modified (or is new)
+    if (!user.isModified('password')) return next();
+
+    // Generate a salt and hash the password using bcrypt
+    bcrypt.hash(user.password, 10, function(error, encrypted) {
+        if (error) return next(error);
+
+        user.password = encrypted;
+        next();
+    });
+});
+
+module.exports = mongoose.model('User', UserSchema);
