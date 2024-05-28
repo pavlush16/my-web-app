90f3fd5db6c318976e93df6f62b027e237f7d7fe
Anastasia Pavlush <n.pavlush16@gmail.com>
2024-05-28 14:17:17 +0300

    saved sessions to mongodb

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const fileUpload = require('express-fileupload');
 const expressSession = require('express-session');
-// const connectMongo = require('connect-mongo');
+const connectMongo = require('connect-mongo');
 
 const createPostController = require('./controllers/createPost');
 const homePageController = require('./controllers/homePage');
@@ -12,21 +12,23 @@ const storePostController = require('./controllers/storePost');
 const getPostController = require('./controllers/getPost');
 const getAboutController = require('./controllers/getAbout');
 const createUserController = require('./controllers/createUser');
-const storeUserController = require('./controllers/storeUser')
-const loginController = require('./controllers/login')
-const loginUserController = require('./controllers/loginUser')
+const storeUserController = require('./controllers/storeUser');
+const loginController = require('./controllers/login');
+const loginUserController = require('./controllers/loginUser');
 
 const app = new express();
 
 mongoose.connect('mongodb://localhost:27017/bloghub');
 
-// const mongoStore = connectMongo(expressSession);
+const mongoStore = connectMongo(expressSession);
 
 app.use(expressSession({
     secret: 'secret',
-    // store: new mongoStore({
-    //     mongooseConnection: mongoose.connection
-    // })
+    resave: false, // Set to false to prevent session being saved on every request
+    saveUninitialized: false, // Set to false to prevent empty sessions from being saved
+    store: new mongoStore({
+        mongooseConnection: mongoose.connection
+    })
 }))
 
 app.use(fileUpload());
@@ -37,7 +39,7 @@ app.set('views', `${__dirname}/views`);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const storePost = require('./middleware/storePost')
+const storePost = require('./middleware/storePost');
 
 app.get('/', homePageController);
 app.get('/posts/new', createPostController);
@@ -51,4 +53,4 @@ app.post('/users/login', loginUserController);
 
 app.listen(4000, () => {
     console.log('App listening on port 4000');
-})
+});
